683e7107c53b9fc796367678cc7c613ec35f9363
blackweeed <blackweeeed@gmail.com>
2023-07-08 16:14:31 +0200

    working on colors

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -16,7 +16,12 @@ module.exports = createCoreController("api::product.product", ({ strapi }) => ({
 
     const entity = await strapi.db.query("api::product.product").findOne({
       where: { slug: id },
-      populate: { images: true },
+      populate: {
+        images: true,
+        subcategories: true,
+        categories: true,
+        colors: true,
+      },
     });
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
