8269f0a7ca5fe6b918837e2843208394db297dbf
blackweeed <blackweeeed@gmail.com>
2023-08-13 10:19:12 +0200

    testing

--- config/plugins.js
+++ config/plugins.js
@@ -1,35 +1,30 @@
 module.exports = ({ env }) => ({
   email: {
-    config: {
-      provider: "nodemailer",
-      providerOptions: {
-        // service: "iCloud",
-        host: env("SMTP_HOST", "smtp.example.com"),
-        port: env("SMTP_PORT", 587),
-        auth: {
-          user: "sklep@levarde.com",
-          pass: "Niewiem1223",
-        },
-        // ... any custom nodemailer options
+    provider: "nodemailer",
+    providerOptions: {
+      host: "smtp.mail.me.com",
+      port: 587,
+      auth: {
+        user: "sklep@levarde.com",
+        pass: "Niewiem1223",
       },
+      // ... any custom nodemailer options
     },
     settings: {
       defaultFrom: "sklep@levarde.com",
-      defaultReplyTo: "sklep@levarde.com ",
+      defaultReplyTo: "sklep@levarde.com",
     },
   },
   upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET,
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET,
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
     },
   },
 });
